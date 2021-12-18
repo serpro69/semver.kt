ca16482bf29c8b12a65e0aadbcaa2af6ee6360d1
Sergio Pro <serpro@disroot.org>
2021-12-18 20:12:16 +0100

    Add gradle build files

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+group = "io.github.serpro69"
+
+dependencies {
+    implementation(kotlin("bom"))
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.0.2")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.0.2")
+    testImplementation("io.kotest:kotest-property-jvm:5.0.2")
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform {}
+}
