7dd4f4a1dc02436bf76c18dabbcb710c9e5b011d
Sergio Pro <serpro@disroot.org>
2022-03-29 13:30:31 +0200

    Remove unused constructors of IllegalVersionException

--- src/main/kotlin/io/github/serpro69/semverkt/IllegalVersionException.kt
+++ src/main/kotlin/io/github/serpro69/semverkt/IllegalVersionException.kt
@@ -1,15 +1,3 @@
 package io.github.serpro69.semverkt
 
-class IllegalVersionException : Throwable {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(cause: Throwable) : super(cause)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(message: String, cause: Throwable, enableSuppression: Boolean, writableStackTrace: Boolean) : super(
-        message,
-        cause,
-        enableSuppression,
-        writableStackTrace
-    )
-}
-
+class IllegalVersionException(message: String) : Throwable(message)
