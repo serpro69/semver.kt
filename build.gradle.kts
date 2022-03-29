1ae59e507fe2bba77cd204cb5c1c63376c1dc121
Sergio Pro <serpro@disroot.org>
2022-03-29 16:17:48 +0200

    Add publishing-related gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.dokka") version "1.5.31"
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -38,4 +41,108 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform {}
+}
+
+val jar by tasks.getting(Jar::class) {
+    archiveBaseName.set(rootProject.name)
+
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Class-Path" to configurations.compileClasspath.get().joinToString(" ") { it.name }
+            )
+        )
+    }
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+    from("LICENCE.md") {
+        into("META-INF")
+    }
+}
+
+val dokkaJavadocJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.get().outputDirectory.orNull)
+}
+
+artifacts {
+    archives(sourcesJar)
+    archives(dokkaJavadocJar)
+}
+
+val artifactName = rootProject.name
+val artifactGroup = project.group.toString()
+val artifactVersion = project.version.toString()
+val releaseTagName = "v$artifactVersion"
+
+val pomUrl = "https://github.com/serpro69/Semver.kt"
+val pomScmUrl = "https://github.com/serpro69/Semver.kt"
+val pomIssueUrl = "https://github.com/serpro69/Semver.kt/issues"
+val pomDesc = "https://github.com/serpro69/Semver.kt"
+
+val ghRepo = "serpro69/Semver.kt"
+val ghReadme = "README.md"
+
+val pomLicenseName = "MIT"
+val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+val pomLicenseDist = "repo"
+
+val pomDeveloperId = "serpro69"
+val pomDeveloperName = "Serhii Prodan"
+
+publishing {
+    publications {
+        create<MavenPublication>("semver") {
+            groupId = artifactGroup
+            artifactId = artifactName
+            version = artifactVersion
+            from(components["java"])
+            artifact(jar)
+            artifact(sourcesJar)
+            artifact(dokkaJavadocJar)
+
+            pom {
+                packaging = "jar"
+                name.set(rootProject.name)
+                description.set(pomDesc)
+                url.set(pomUrl)
+                scm {
+                    url.set(pomScmUrl)
+                }
+                issueManagement {
+                    url.set(pomIssueUrl)
+                }
+                licenses {
+                    license {
+                        name.set(pomLicenseName)
+                        url.set(pomLicenseUrl)
+                    }
+                }
+                developers {
+                    developer {
+                        id.set(pomDeveloperId)
+                        name.set(pomDeveloperName)
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    if (!version.toString().endsWith("SNAPSHOT")) {
+        sign(publishing.publications["semver"])
+    }
+}
+
+tasks {
+    assemble {
+        dependsOn(jar)
+    }
 }
