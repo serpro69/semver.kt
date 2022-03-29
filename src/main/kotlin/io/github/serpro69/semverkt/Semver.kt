77150b9dd7a3e5d2ea1b4a7e949b4153c923a171
Sergio Pro <serpro@disroot.org>
2022-03-29 12:00:59 +0200

    Implement compareTo, equals, hashCode and isValid functions

--- src/main/kotlin/io/github/serpro69/semverkt/Semver.kt
+++ src/main/kotlin/io/github/serpro69/semverkt/Semver.kt
@@ -2,10 +2,13 @@
 
 package io.github.serpro69.semverkt
 
+import java.util.regex.Pattern
 import kotlin.math.sign
 
 class Semver(private val version: String) : Comparable<Semver> {
-    val normalVersion: Semver
+    private val versionPattern: Pattern =
+        Pattern.compile("""^((?:\d+\.?){3})(-(?:[0-9A-Za-z-]*.?)*)?(\+(?:[0-9A-Za-z-]*.?)*)?${'$'}""")
+    val normalVersion: String = version.substringBefore("-").substringBefore("+")
     val preRelease: PreRelease?
     val buildMetadata: BuildMetadata?
     val major: Int
@@ -14,11 +17,10 @@ class Semver(private val version: String) : Comparable<Semver> {
 
     init {
         if (!isValid()) throw IllegalVersionException("'$version' is not a valid semver version.")
-        normalVersion = Semver(version.substringBefore("-").substringBefore("+")).also {
-            val v = it.version.split(".")
-            major = v[0].toInt()
-            minor = v[1].toInt()
-            patch = v[2].toInt()
+        normalVersion.split(".").also {
+            major = it[0].toInt()
+            minor = it[1].toInt()
+            patch = it[2].toInt()
         }
         PreRelease(version.substringAfter("-", "").takeWhile { it != '+' }).also {
             preRelease = if (it.value == "") null else it
@@ -63,37 +65,93 @@ class Semver(private val version: String) : Comparable<Semver> {
     fun incrementPatch() = Semver(major, minor, patch + 1)
 
     override operator fun compareTo(other: Semver): Int {
-        TODO("Not yet implemented")
+        val compareNormal = if (major > other.major) 1 else if (major < other.major) -1 else {
+            if (minor > other.minor) 1 else if (minor < other.minor) -1 else {
+                if (patch > other.patch) 1 else if (patch < other.patch) -1 else 0
+            }
+        }
+        return when (compareNormal) {
+            0 -> {
+                when {
+                    preRelease != null -> if (other.preRelease != null) preRelease.compareTo(other.preRelease) else -1
+                    other.preRelease != null -> 1
+                    else -> compareNormal
+                }
+            }
+            else -> compareNormal
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Semver -> major == other.major && minor == other.minor && patch == other.patch && preRelease == other.preRelease
+            else -> false
+        }
     }
 
     override fun toString(): String = version
 
+    override fun hashCode(): Int {
+        var result = major
+        result = 31 * result + minor
+        result = 31 * result + patch
+        result = 31 * result + (preRelease?.hashCode() ?: 0)
+        result = 31 * result + (buildMetadata?.hashCode() ?: 0)
+        return result
+    }
+
     private fun isValid(): Boolean {
-        val elements = version.split(".")
-            .map {
-                when {
-                    it.length > 1 && it.startsWith("0") -> {
-                        throw IllegalVersionException("'$version' numbers MUST NOT contain leading zeroes.")
-                    }
-                    else -> it.toInt()
+        normalVersion.split(".").map {
+            when {
+                it.length > 1 && it.startsWith("0") -> {
+                    throw IllegalVersionException("'$version' numbers MUST NOT contain leading zeroes.")
+                }
+                it.toInt().sign == -1 -> {
+                    throw IllegalVersionException("'$version' numbers MUST NOT not be negative.")
+                }
+                else -> { /*NOOP*/
                 }
             }
-        return elements.none { it.sign == -1 } && elements.size == 3
+        }
+
+        return version.matches(versionPattern.toRegex())
     }
 }
 
 class PreRelease internal constructor(val value: String) : Comparable<PreRelease> {
     override fun toString(): String = value
 
-    override fun compareTo(other: PreRelease): Int {
-        TODO("Not yet implemented")
+    override operator fun compareTo(other: PreRelease): Int {
+        return value.compareTo(other.value)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is PreRelease -> value == other.value
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
     }
 }
 
-class BuildMetadata internal constructor(val value: String) : Comparable<PreRelease> {
+class BuildMetadata internal constructor(val value: String) : Comparable<BuildMetadata> {
     override fun toString(): String = value
 
-    override fun compareTo(other: PreRelease): Int {
-        TODO("Not yet implemented")
+    override operator fun compareTo(other: BuildMetadata): Int {
+        return value.compareTo(other.value)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is BuildMetadata -> value == other.value
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
     }
 }
