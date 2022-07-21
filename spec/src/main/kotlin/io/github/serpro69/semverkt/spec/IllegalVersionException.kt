2f1df8bf1802baf9f2625d1442404a71692f5473
Serhii Prodan <serpro@disroot.org>
2022-07-21 11:45:37 +0200

    Change IllegalVersionException class visibility to public

--- spec/src/main/kotlin/io/github/serpro69/semverkt/spec/IllegalVersionException.kt
+++ spec/src/main/kotlin/io/github/serpro69/semverkt/spec/IllegalVersionException.kt
@@ -1,3 +1,20 @@
+@file:Suppress("unused")
+
 package io.github.serpro69.semverkt.spec
 
-internal class IllegalVersionException(message: String) : Throwable(message)
+class IllegalVersionException : Throwable {
+
+    constructor(message: String) : super(message)
+
+    constructor(cause: Throwable) : super(cause)
+
+    constructor(message: String, cause: Throwable) : super(message, cause)
+
+    constructor(
+        message: String,
+        cause: Throwable,
+        enableSuppression: Boolean,
+        writableStackTrace: Boolean
+    ) : super(message, cause, enableSuppression, writableStackTrace)
+}
+
