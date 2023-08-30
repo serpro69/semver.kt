b959ec20b45cd993f73146cb5d259b059a07a499
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-08-30 14:38:08 +0200

    Fix whitespace in functionalTest sources

--- gradle-plugin/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/gradle/Builder.kt
+++ gradle-plugin/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/gradle/Builder.kt
@@ -10,61 +10,61 @@ import java.nio.file.Path
 
 object Builder {
 
-  @JvmOverloads
-  @JvmStatic
-  fun build(
-      gradleVersion: GradleVersion = GradleVersion.current(),
-      project: AbstractProject,
-      withPluginClasspath: Boolean = true,
-      vararg args: String
-  ): BuildResult = runner(gradleVersion, project.projectDir, withPluginClasspath, *args).build()
+    @JvmOverloads
+    @JvmStatic
+    fun build(
+        gradleVersion: GradleVersion = GradleVersion.current(),
+        project: AbstractProject,
+        withPluginClasspath: Boolean = true,
+        vararg args: String
+    ): BuildResult = runner(gradleVersion, project.projectDir, withPluginClasspath, *args).build()
 
-  @JvmOverloads
-  @JvmStatic
-  fun build(
-    gradleVersion: GradleVersion = GradleVersion.current(),
-    projectDir: Path,
-    withPluginClasspath: Boolean = true,
-    vararg args: String
-  ): BuildResult = runner(gradleVersion, projectDir, withPluginClasspath, *args).build()
+    @JvmOverloads
+    @JvmStatic
+    fun build(
+        gradleVersion: GradleVersion = GradleVersion.current(),
+        projectDir: Path,
+        withPluginClasspath: Boolean = true,
+        vararg args: String
+    ): BuildResult = runner(gradleVersion, projectDir, withPluginClasspath, *args).build()
 
-  @JvmOverloads
-  @JvmStatic
-  fun buildAndFail(
-      gradleVersion: GradleVersion = GradleVersion.current(),
-      project: AbstractProject,
-      withPluginClasspath: Boolean = true,
-      vararg args: String
-  ): BuildResult = runner(
-    gradleVersion,
-    project.projectDir,
-    withPluginClasspath,
-    *args
-  ).buildAndFail()
+    @JvmOverloads
+    @JvmStatic
+    fun buildAndFail(
+        gradleVersion: GradleVersion = GradleVersion.current(),
+        project: AbstractProject,
+        withPluginClasspath: Boolean = true,
+        vararg args: String
+    ): BuildResult = runner(
+        gradleVersion,
+        project.projectDir,
+        withPluginClasspath,
+        *args
+    ).buildAndFail()
 
-  @JvmOverloads
-  @JvmStatic
-  fun buildAndFail(
-    gradleVersion: GradleVersion = GradleVersion.current(),
-    projectDir: Path,
-    withPluginClasspath: Boolean = true,
-    vararg args: String
-  ): BuildResult = runner(gradleVersion, projectDir, withPluginClasspath, *args).buildAndFail()
+    @JvmOverloads
+    @JvmStatic
+    fun buildAndFail(
+        gradleVersion: GradleVersion = GradleVersion.current(),
+        projectDir: Path,
+        withPluginClasspath: Boolean = true,
+        vararg args: String
+    ): BuildResult = runner(gradleVersion, projectDir, withPluginClasspath, *args).buildAndFail()
 
-  private fun runner(
-    gradleVersion: GradleVersion,
-    projectDir: Path,
-    withPluginClasspath: Boolean = true,
-    vararg args: String
-  ): GradleRunner = GradleRunner.create().apply {
-    forwardOutput()
-    if (withPluginClasspath) {
-      withPluginClasspath()
+    private fun runner(
+        gradleVersion: GradleVersion,
+        projectDir: Path,
+        withPluginClasspath: Boolean = true,
+        vararg args: String
+    ): GradleRunner = GradleRunner.create().apply {
+        forwardOutput()
+        if (withPluginClasspath) {
+            withPluginClasspath()
+        }
+        withGradleVersion(gradleVersion.version)
+        withProjectDir(projectDir.toFile())
+        withArguments(args.toList() + "-s")
+        // Ensure this value is true when `--debug-jvm` is passed to Gradle, and false otherwise
+        withDebug(getRuntimeMXBean().inputArguments.toString().indexOf("-agentlib:jdwp") > 0)
     }
-    withGradleVersion(gradleVersion.version)
-    withProjectDir(projectDir.toFile())
-    withArguments(args.toList() + "-s")
-    // Ensure this value is true when `--debug-jvm` is passed to Gradle, and false otherwise
-    withDebug(getRuntimeMXBean().inputArguments.toString().indexOf("-agentlib:jdwp") > 0)
-  }
 }
