d02d0427cb137df880e109013cdf551c5663488d
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-08-30 14:30:53 +0200

    Add assertions to functional test

--- gradle-plugin/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/SemverKtPluginFT.kt
+++ gradle-plugin/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/SemverKtPluginFT.kt
@@ -3,14 +3,17 @@ package io.github.serpro69.semverkt.gradle.plugin
 import io.github.serpro69.semverkt.gradle.plugin.fixture.SemverKtTestProject
 import io.github.serpro69.semverkt.gradle.plugin.gradle.Builder
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.gradle.testkit.runner.TaskOutcome
 
 class SemverKtPluginFT : DescribeSpec({
 
-    describe("gradle-semver-release plugin") {
+    describe("gradle plugin semver-release") {
         it("test me") {
             val result = Builder.build(project = SemverKtTestProject(), args = arrayOf("ft"))
-
-            println(result.output)
+            result.task(":ft")?.outcome shouldBe TaskOutcome.NO_SOURCE
+            result.output shouldContain "Project version: 1.2.3"
         }
     }
 })
