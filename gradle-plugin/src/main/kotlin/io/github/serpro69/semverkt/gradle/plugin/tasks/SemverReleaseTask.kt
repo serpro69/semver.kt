d4ccb9ea8dc27ed206d4755d5e54556ff0e9b7a2
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-09-24 09:34:10 +0200

    Remove plugin extension from project-level
    Can't really configure plugin via build file since it's a settings
    plugin and not a project plugin, hence extension on project doesn't
    make much sense.
    
    Also fix casts to Semver for cases where no (previous) versions exist (new repos)

--- gradle-plugin/src/main/kotlin/io/github/serpro69/semverkt/gradle/plugin/tasks/SemverReleaseTask.kt
+++ gradle-plugin/src/main/kotlin/io/github/serpro69/semverkt/gradle/plugin/tasks/SemverReleaseTask.kt
@@ -1,6 +1,14 @@
 package io.github.serpro69.semverkt.gradle.plugin.tasks
 
+import io.github.serpro69.semverkt.spec.Semver
 import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Internal
 
 abstract class SemverReleaseTask : DefaultTask() {
+
+    @get:Internal
+    internal abstract val latestVersion: Property<Semver>
+    @get:Internal
+    internal abstract val nextVersion: Property<Semver>
 }
--- gradle-plugin/src/main/kotlin/io/github/serpro69/semverkt/gradle/plugin/tasks/TagTask.kt
+++ gradle-plugin/src/main/kotlin/io/github/serpro69/semverkt/gradle/plugin/tasks/TagTask.kt
@@ -1,14 +1,10 @@
 package io.github.serpro69.semverkt.gradle.plugin.tasks
 
-import io.github.serpro69.semverkt.release.Increment.*
-import io.github.serpro69.semverkt.release.SemverRelease
-import io.github.serpro69.semverkt.release.configuration.Configuration
-import io.github.serpro69.semverkt.spec.Semver
 import org.eclipse.jgit.api.Git
-import org.gradle.api.logging.LogLevel.*
+import org.gradle.api.logging.LogLevel.DEBUG
+import org.gradle.api.logging.LogLevel.LIFECYCLE
 import org.gradle.api.logging.Logging
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
@@ -16,11 +12,7 @@ abstract class TagTask : SemverReleaseTask() {
     private val logger = Logging.getLogger(this::class.java)
 
     @get:Internal
-    internal abstract val latestVersion: Property<Semver>
-    @get:Internal
-    internal abstract val nextVersion: Property<Semver>
-    @get:Internal
-    internal val dryRun = project.hasProperty("dryRun")
+    internal abstract val dryRun: Property<Boolean>
 
     @TaskAction
     fun tag() {
@@ -29,7 +21,7 @@ abstract class TagTask : SemverReleaseTask() {
         logger.log(DEBUG, "nextVersion: $next")
         if (next == latest) {
             logger.log(LIFECYCLE, "latestVersion == nextVersion, not doing anything")
-        } else if (!dryRun) {
+        } else if (!dryRun.get() && next != null) {
             with(project.buildFile.parentFile) {
                 logger.log(DEBUG, "Open repo at: $this")
                 val repo = Git.open(this)
