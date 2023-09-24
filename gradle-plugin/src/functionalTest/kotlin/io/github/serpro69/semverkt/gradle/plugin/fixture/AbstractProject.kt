f05b9d4f36c4cb40927a1223ca844814139618b3
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-09-24 12:10:24 +0200

    Fix functional test

--- gradle-plugin/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/fixture/AbstractProject.kt
+++ gradle-plugin/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/fixture/AbstractProject.kt
@@ -1,13 +1,23 @@
 package io.github.serpro69.semverkt.gradle.plugin.fixture
 
+import org.eclipse.jgit.api.Git
 import java.nio.file.Path
 import java.util.UUID
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
+import kotlin.io.path.createFile
+import kotlin.io.path.writeText
 
 abstract class AbstractProject : AutoCloseable {
 
-    val projectDir = Path("build/functionalTest/${slug()}").createDirectories()
+    val projectDir = Path("build/functionalTest/${slug()}").createDirectories().also {
+        it.resolve("README.md").createFile().writeText("Hello, world")
+        Git.init().setDirectory(it.toFile()).call()
+        Git.open(it.toFile()).use { git ->
+            git.add().addFilepattern("README.md").call()
+            git.commit().setMessage("init").call()
+        }
+    }
     private val buildDir = projectDir.resolve("build")
 
     fun buildFile(filename: String): Path {
