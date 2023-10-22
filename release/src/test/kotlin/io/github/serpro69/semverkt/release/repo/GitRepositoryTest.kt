87f3b7c7ea9ed758a30728fdbdec592276dffa21
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-10-22 12:33:30 +0200

    Improve error message for git initialization of GitRepository

---
+++ release/src/test/kotlin/io/github/serpro69/semverkt/release/repo/GitRepositoryTest.kt
@@ -0,0 +1,29 @@
+package io.github.serpro69.semverkt.release.repo
+
+import io.github.serpro69.semverkt.release.configuration.PropertiesConfiguration
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.eclipse.jgit.errors.RepositoryNotFoundException
+import java.io.IOException
+import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+
+class GitRepositoryTest : DescribeSpec({
+
+    assertSoftly = true
+
+    describe("git repository") {
+        it("should throw exception of directory is not a git repo") {
+            val dir = Path("build/test/resources/does-not-exist")
+            val testProperties = Properties().also {
+                it["git.repo.directory"] = dir
+            }
+            val testConfiguration = PropertiesConfiguration(testProperties)
+            val ex = shouldThrow<IOException> { GitRepository(testConfiguration).tags() }
+            ex.message shouldBe "Can't open $dir as git repository"
+            ex.cause shouldBe RepositoryNotFoundException(dir.absolutePathString())
+        }
+    }
+})
