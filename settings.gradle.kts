37c54cb04d0084b077ca4b56af130a970889bac5
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-12-19 09:01:31 +0100

    Bump version of self

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.github.serpro69.semantic-versioning") version "0.7.1"
+    id("io.github.serpro69.semantic-versioning") version "0.8.0"
 }
 
 rootProject.name = "semver.kt"
@@ -27,8 +27,8 @@ settings.extensions.configure<SemverPluginExtension>("semantic-versioning") {
         }
     }
     monorepo {
-      module("release") {}
-      module("semantic-versioning") {}
-      module("spec") {}
+        module("release") {}
+        module("semantic-versioning") {}
+        module("spec") {}
     }
 }
