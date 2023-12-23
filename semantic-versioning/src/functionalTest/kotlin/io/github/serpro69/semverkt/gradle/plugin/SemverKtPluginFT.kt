4a9c92a3603593d356c5d0344615be6ae71aedf4
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-12-23 18:07:36 +0100

    Add func-test to check compatibility with gradle 7.5

--- semantic-versioning/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/SemverKtPluginFT.kt
+++ semantic-versioning/src/functionalTest/kotlin/io/github/serpro69/semverkt/gradle/plugin/SemverKtPluginFT.kt
@@ -545,6 +545,27 @@ class SemverKtPluginFT : DescribeSpec({
     }
 
     describe("compatible gradle version") {
+        context("gradle 7.5") {
+            it("should be compatible") {
+                val project = SemverKtTestProject()
+                // Arrange
+                Git.open(project.projectDir.toFile()).use {
+                    project.projectDir.resolve("text.txt").createFile().writeText("Hello")
+                    it.add().addFilepattern("text.txt").call()
+                    it.commit().setMessage("New [minor] version").call()
+                }
+                // Act
+                val result = Builder.build(
+                    gradleVersion = GradleVersion.version("7.5"),
+                    project = project,
+                    args = arrayOf("tag", "-PdryRun")
+                )
+                // Assert
+                result.task(":tag")?.outcome shouldBe TaskOutcome.SUCCESS
+                // initial version should be calculated from config, so the keyword value doesn't really matter so long as it's valid
+                result.output shouldContain "Calculated next version: 0.1.0"
+            }
+        }
         // gradle 8.x
         (0..5).forEach { minorVer ->
             context("gradle 8.$minorVer") {
@@ -571,13 +592,13 @@ class SemverKtPluginFT : DescribeSpec({
         }
     }
 
-    // test is broken because plugin-under-test.metadata file contains effective classpath of the plugin,
-    // and hence even though we run with gradle 7.x, classpath contains kotlin 1.9.0,
-    // which is the reason why gradle 7.x is incompatible in the first place
-    // not sure yet how to make this work in an automated way
+    // Test is broken because the test is executed with gradle8,
+    // and hence the attribute org.gradle.plugin.api-version is set to the gradle runtime,
+    // and not the version of gradle build runner.
+    // Not sure yet how to make this work
     describe("!incompatible gradle version") {
-        // gradle 7.x
-        context("gradle 7.x") {
+        // latest incompatible gradle version
+        context("gradle 7.4") {
             // disable soft assertions to fail-fast in Arrange
             assertSoftly = false
 
@@ -586,7 +607,7 @@ class SemverKtPluginFT : DescribeSpec({
                 // - run gradle task with default settings
                 val project = SemverKtTestProject(defaultSettings = true)
                 Builder.build(
-                    gradleVersion = GradleVersion.version("7.6.3"), // latest 7.x version
+                    gradleVersion = GradleVersion.version("7.4"),
                     project = project,
                     args = arrayOf("clean")
                     // just add some dumb assertion to verify gradle actually ran successfully
@@ -601,12 +622,13 @@ class SemverKtPluginFT : DescribeSpec({
                 }
                 // Act
                 val result = Builder.buildAndFail(
-                    gradleVersion = GradleVersion.version("7.6.3"),
+                    gradleVersion = GradleVersion.version("7.4"),
                     project = project,
                     args = arrayOf("tag", "-PdryRun")
                 )
                 // Assert
                 result.task(":tag")?.outcome shouldBe null // settings compilation should fail
+                result.output shouldContain "No matching variant of io.github.serpro69:semantic-versioning:0.0.0 was found"
                 result.output shouldNotContain "Calculated next version"
             }
 
